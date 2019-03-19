58359f67e5654171e4cc5935b3b5eb23f71248d6
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-19 18:16:07 +0000

    1st chalenge finished

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,15 @@
-console.log("hello world!!");
+// 1st challenge
+
+var massJohn, massMark, heightJohn, heightMark;
+massJohn = 80;
+massMark = 94;
+
+heightJohn = 1.84;
+heightMark = 1.84;
+
+var bmiJohn = massJohn / (heightJohn * heightJohn);
+var bmiMark = massMark / (heightMark * heightMark);
+
+bmiMarkHigher =  bmiMark > bmiJohn;
+
+console.log("Is Mark's BMI higher than John's? " + bmiMarkHigher )
