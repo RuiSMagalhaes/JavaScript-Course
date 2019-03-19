caa7f59bf7b87688f886cb0b77725af3b14aa5c5
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-19 19:06:23 +0000

    2nd challenge done

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,5 +1,7 @@
 // 1st challenge
 
+console.log("1st challenge");
+
 var massJohn, massMark, heightJohn, heightMark;
 massJohn = 80;
 massMark = 94;
@@ -14,3 +16,31 @@ console.log(bmiJohn ,bmiMark);
 bmiMarkHigher =  bmiMark > bmiJohn;
 
 console.log("Is Mark's BMI higher than John's? " + bmiMarkHigher )
+
+// changing challenge
+console.log("1st challenge finished!!");
+
+
+// 2nd challenge
+
+console.log("2nd challenge");
+
+var avgJohn = (116 + 94 + 123) / 3;
+var avgMark = (89 + 120 + 103) / 3;
+var avgMary = (97 + 134 + 105) /3
+console.log(avgJohn, avgMark, avgMary);
+
+if (avgJohn > avgMark && avgJohn > avgMary) {
+  console.log("John's team won");
+{ else if (avgMark > avgJohn && avgMark > avgMary) {
+  console.log("Mark's team won");
+{ else if (avgMary > avgJohn && avgMary > avgMark) {
+  console.log("Mary's team won");
+} else if (avgJohn === avgMark === avgMary){
+  console.log(" It's a draw!");
+} else  {
+  console.log("Someone have a draw with another player");
+};
+
+// changing challenge
+console.log("2nd challenge finished!!");
