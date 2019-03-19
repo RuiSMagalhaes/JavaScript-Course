6e0efe14f1ad1adc9800306c98157b92e191bbd5
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-19 18:20:09 +0000

    1st chalenge finished. added a console log to check the bmi variables of john and mark

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -9,6 +9,7 @@ heightMark = 1.84;
 
 var bmiJohn = massJohn / (heightJohn * heightJohn);
 var bmiMark = massMark / (heightMark * heightMark);
+console.log(bmiJohn ,bmiMark);
 
 bmiMarkHigher =  bmiMark > bmiJohn;
 
