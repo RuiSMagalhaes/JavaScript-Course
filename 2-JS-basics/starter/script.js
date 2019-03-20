c0c689143d51963629120268dae1535e3125d102
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-20 14:37:36 +0000

    3rd challenge finished. error of 2nd one corrected

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -32,9 +32,9 @@ console.log(avgJohn, avgMark, avgMary);
 
 if (avgJohn > avgMark && avgJohn > avgMary) {
   console.log("John's team won");
-{ else if (avgMark > avgJohn && avgMark > avgMary) {
+} else if (avgMark > avgJohn && avgMark > avgMary) {
   console.log("Mark's team won");
-{ else if (avgMary > avgJohn && avgMary > avgMark) {
+} else if (avgMary > avgJohn && avgMary > avgMark) {
   console.log("Mary's team won");
 } else if (avgJohn === avgMark === avgMary){
   console.log(" It's a draw!");
@@ -44,3 +44,35 @@ if (avgJohn > avgMark && avgJohn > avgMary) {
 
 // changing challenge
 console.log("2nd challenge finished!!");
+
+
+// 3rd challenge
+
+console.log("3rd challenge");
+
+tips = [];
+total = [];
+
+function calculator(bill){
+  if (bill < 0) {
+    tip = 0;
+  } else if (bill < 50) {
+    tip = bill * .2;
+  } else if (bill >= 50 && bill <= 200) {
+    tip =  bill * .15;
+  } else {
+    tip = bill * .10;
+  };
+  tips.push(tip);
+  total.push(bill + tip);
+};
+
+
+calculator(124);
+calculator(48);
+calculator(268);
+
+console.log(tips, total);
+
+console.log("3rd challenge finished!!");
+
