5af724e43d927053235e38141e357c14829c5067
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-05-06 17:00:22 +0100

    expert level done

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -9,20 +9,49 @@ var secondQuestion = new Question("Quem é o Presidente da Republica?", ["Marcel
 var thirdQuestion = new Question("Qual o melhor bootCamp de programação do mundo?", ["IronHack","Outro Qualquer", "Le Wagon"], 2)
 
 questionArray = [firstQuestion, secondQuestion, thirdQuestion];
+var score = 0;
 
 init();
 
 function init() {
   questionSelected = questionArray[Math.round(Math.random()*2)];
-  console.log(questionSelected.question);
+  consoleQuestion(questionSelected);
+  consoleAnswers(questionSelected);
 
+  var userAnswer = prompt("Please select the correct answer (Just type the number)");
+  checkAnswer(userAnswer);
+
+}
+
+function checkAnswer(userAnswer) {
+  if (userAnswer = "exit") {
+    break;
+  }
+  if (userAnswer == questionSelected.correctAnswer ) {
+    console.log("correct answer! Congrats :)");
+    score ++;
+    console.log ("Your current score is: " + score);
+    console.log ("**********************************************");
+    init();
+  } else
+    console.log("WRONG answer! Try again! ")
+    console.log ("Your current score is: " + score);
+    console.log ("**********************************************");
+    consoleQuestion(questionSelected);
+    consoleAnswers(questionSelected);
+    userAnswer = prompt("Please select the correct answer (Just type the number)");
+    checkAnswer(userAnswer);
+
+}
+
+function consoleQuestion(questionSelected) {
+  console.log(questionSelected.question);
+}
+
+function consoleAnswers(questionSelected) {
   for (var i = 0; i <  questionSelected.answers.length ; i++) {
     console.log(i +": " + questionSelected.answers[i]);
   }
-
-  var userAnswer = prompt("Please select the correct answer (Just type the number)");
-  if (userAnswer == questionSelected.correctAnswer ) {
-    console.log("correct answer! Congrats :)")
-  } else
-    console.log("WRONG answer! Sorry :(")
 }
+
+
